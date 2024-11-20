53fe2f1aa40ac6397e083680602da175e4967042
Billazbong <alexis_loyez@ens.univ-artois.fr>
2024-11-20 23:32:57 +0100

    Added A* algorithm and manhattan distance calculation

---
+++ algorithm.py
@@ -0,0 +1,80 @@
+from collections import deque
+import heapq
+
+def bfs(taquin):
+    queue=deque([(taquin.tableau,None)])
+    visited=set([taquin.tableau])
+    parent_dict={taquin.tableau:None}
+    while queue:
+        current_state,parent_state=queue.popleft()
+        if taquin.final(current_state):
+            path=[current_state]
+            while parent_state is not None:
+                path.append(parent_state)
+                parent_state=parent_dict[parent_state]
+            path.reverse()
+            return path
+
+        for state in taquin.generate_next_states(current_state):
+            if state not in visited:
+                visited.add(state)
+                parent_dict[state]=current_state
+                queue.append((state,current_state))
+
+def dfs(taquin):
+    stack=[(taquin.tableau,None)]
+    visited=set([taquin.tableau])
+    parent_dict={taquin.tableau:None}
+    while stack:
+        current_state,parent_state=stack.pop()
+        if taquin.final(current_state):
+            path=[current_state]
+            while parent_state is not None:
+                path.append(parent_state)
+                parent_state=parent_dict[parent_state]
+            path.reverse()
+            return path
+
+
+        for state in taquin.generate_next_states(current_state):
+            if state not in visited:
+                visited.add(state)
+                parent_dict[state]=current_state
+                stack.append((state,current_state))
+
+def manhattan_distance(state,k):
+    """Calculate the sum of all manhattan distances"""
+    distance=0
+    for i in range(k):
+        for j in range(k):
+            value=state[i*k+j-1]
+            if value!=0:
+                goal_x,goal_y=divmod(value-1,k)
+                distance+= abs(i-goal_x)+abs(j-goal_y)
+    return distance
+
+def a_star(taquin):
+    """Implement A* algorithm to solve taquin"""
+    priority_queue=[]
+    heapq.heappush(priority_queue,(0,taquin.tableau,0,None)) #f(n), state, g(n), parent_state
+    visited=set([taquin.tableau])
+    parent_dict={taquin.tableau:None}
+    while priority_queue:
+        _,current_state,g,parent_state=heapq.heappop(priority_queue)
+        if taquin.final(current_state):
+            path=[current_state]
+            while parent_state is not None:
+                path.append(parent_state)
+                parent_state=parent_dict[parent_state]
+            path.reverse()
+            return path
+        for state in taquin.generate_next_states(current_state):
+            if state not in visited:
+                new_g=g+1
+                new_f=new_g+manhattan_distance(state,taquin.k)
+                visited.add(state)
+                parent_dict[state]=current_state
+                heapq.heappush(priority_queue,(new_f,state,new_g,current_state))
+
+
+
--- taquin.py
+++ taquin.py
@@ -1,7 +1,7 @@
 import sys
 import random
-from collections import deque
 import tkinter
+import algorithm
 import time
 
 class Taquin :
@@ -9,7 +9,7 @@ class Taquin :
 
     def __init__(self,k):
         self.solution=tuple([(j + 1) % (k * k) for j in range(k*k)])
-        self.len=k
+        self.k=k
         self.tableau=[]
         resolvable=False
         while not resolvable:
@@ -20,11 +20,11 @@ class Taquin :
 
 
     def generate(self):
-        self.tableau=[0 for _ in range(self.len*self.len)]
-        for i in range (self.len*self.len):
+        self.tableau=[0 for _ in range(self.k*self.k)]
+        for i in range (self.k*self.k):
             done=0
             while (done==0):
-                x=random.randint(0,self.len*self.len-1)
+                x=random.randint(0,self.k*self.k-1)
                 if (self.tableau[x]==0):
                     self.tableau[x]=i
                     done=1
@@ -32,9 +32,9 @@ class Taquin :
     def solvable(self):
         swap=0
         copytableau=[x for x in self.tableau if x != 0]
-        for i in range(self.len*self.len-1):
+        for i in range(self.k*self.k-1):
             if copytableau[i]!=i+1:
-                for j in range(i+1,self.len*self.len-1):
+                for j in range(i+1,self.k*self.k-1):
                     if copytableau[j]==i+1:
                         copytableau[j],copytableau[i]=copytableau[i],copytableau[j]
                         swap+=1
@@ -50,72 +50,36 @@ class Taquin :
         next_states=[]
         empty_pos=(0,0)
 
-        for i in range(self.len):
-            for j in range(self.len):
-                if (current_state[i*self.len+j]==0):
+        for i in range(self.k):
+            for j in range(self.k):
+                if (current_state[i*self.k+j]==0):
                     empty_pos=(i,j)
                     break
 
         for dx,dy in Taquin.directions:
             x,y=empty_pos[0]+dx,empty_pos[1]+dy
-            if  0<=x<self.len and 0<=y<self.len:
+            if  0<=x<self.k and 0<=y<self.k:
                 new_state=current_state[:]
-                new_state[empty_pos[0]*self.len+empty_pos[1]],new_state[x*self.len+y]=new_state[x*self.len+y],new_state[empty_pos[0]*self.len+empty_pos[1]]
+                new_state[empty_pos[0]*self.k+empty_pos[1]],new_state[x*self.k+y]=new_state[x*self.k+y],new_state[empty_pos[0]*self.k+empty_pos[1]]
                 next_states.append(tuple(new_state))
 
         return next_states
 
-def bfs(taquin):
-    queue=deque([(taquin.tableau,None)])
-    visited=set([taquin.tableau])
-    parent_dict={taquin.tableau:None}
-    while queue:
-        current_state,parent_state=queue.popleft()
-        if taquin.final(current_state):
-            path=[current_state]
-            while parent_state is not None:
-                path.append(parent_state)
-                parent_state=parent_dict[parent_state]
-            path.reverse()
-            return path
 
-        for state in taquin.generate_next_states(current_state):
-            if state not in visited:
-                visited.add(state)
-                parent_dict[state]=current_state
-                queue.append((state,current_state))
-
-def dfs(taquin):
-    stack=[(taquin.tableau,None)]
-    visited=set([taquin.tableau])
-    parent_dict={taquin.tableau:None}
-    while stack:
-        current_state,parent_state=stack.pop()
-        if taquin.final(current_state):
-            path=[current_state]
-            while parent_state is not None:
-                path.append(parent_state)
-                parent_state=parent_dict[parent_state]
-            path.reverse()
-            return path
-
-
-        for state in taquin.generate_next_states(current_state):
-            if state not in visited:
-                visited.add(state)
-                parent_dict[state]=current_state
-                stack.append((state,current_state))
-
-
-def test(algorithme,k):
-    for _ in range(1000):
+def test(algorithme,k,n):
+    for _ in range(n):
         taquin=Taquin(k)
         if algorithme(taquin) is None :
             return False
+    return True
 
 
 
 
-print(test(bfs,3))
-print("--------------------------------------------------------------")
-print(test(dfs,3))
+#print("--------------------------------------------------------------")
+#print(test(algorithm.bfs,3,10))
+#print("--------------------------------------------------------------")
+#print(test(algorithm.dfs,3,10))
+a=Taquin(3)
+print(algorithm.a_star(a))
+
